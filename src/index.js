a575c500eab3498fcf366a3b30ab4796d96a088b
Jason Park <zeipar@gmail.com>
2020-11-10 01:53:56 -0800

    Stored conditional strings as string variables.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
+number.innerText = 0;
+
+const ADD = "Add";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 };
 
 const countStore = createStore(countModifier);
@@ -25,11 +32,11 @@ countStore.subscribe(onChange);
 // };
 //
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", () => handleAdd());
