683b681076aeccbe7e1a230c6d27277937a2d4de
Jason Park <zeipar@gmail.com>
2020-11-12 14:47:39 -0800

    Created lists with button to delete, and refactored to modularize event/dispatch/logic.

--- src/index.js
+++ src/index.js
@@ -7,10 +7,24 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
       return [];
     default:
@@ -23,17 +37,37 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
-const createTodo = toDo => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const dispatchAddToDo = text => {
+  store.dispatch(addTodo(text));
 };
+
+const dispatchDeleteTodo = e => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteTodo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    button.innerText = "DEL";
+    button.addEventListener("click", dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
