d7508c61bbaf74fcd95001d5e88ea8e071bd3053
Jason Park <zeipar@gmail.com>
2020-11-12 00:29:34 -0800

    Made the to-do list the index.js to be run as default. Added reducer and used dispatch.

---
+++ src/counter.js
@@ -0,0 +1,43 @@
+import { createStore } from "redux";
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.getElementById("number");
+
+number.innerText = 0;
+
+const ADD = "Add";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+// const updateText = () => {
+//   number.innerText = count;
+// };
+//
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", () => handleAdd());
+minus.addEventListener("click", () => handleMinus());
