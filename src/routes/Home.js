871d6922c0c056f0bd34da2eae8bc927761d3552
Jason Park <zeipar@gmail.com>
2020-11-13 10:58:51 -0800

    Created a simple submit form.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,22 @@
-export default () => "Home";
+import React, { useState } from "react";
+
+export default function Home() {
+  const [text, setText] = useState("");
+  const onChange = e => {
+    setText(e.target.value);
+  };
+  const onSumbit = e => {
+    console.log("submited!");
+    e.preventDefault();
+  };
+  return (
+    <>
+      <h1>To do</h1>
+      <form onSubmit={onSumbit}>
+        <input type="text" value={text} onChange={onChange} />
+        <input type="submit" value="Add" />
+      </form>
+      <ul></ul>
+    </>
+  );
+}
