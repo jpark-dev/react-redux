4ddfd4072ad3652ff0bfca3cbd22bd4089b52570
Jason Park <zeipar@gmail.com>
2020-11-14 22:38:00 -0800

    Used createReducer() to refactor reducer.

--- src/store.js
+++ src/store.js
@@ -1,20 +1,31 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== parseInt(action.payload));
+//     default:
+//       return state;
+//   }
+// };
 
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== parseInt(action.payload));
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    // not returning as the state is being mutated.
+    state.unshift({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    // return if a new state is created.
+    return state.filter(toDo => toDo.id !== parseInt(action.payload));
+  },
+});
 
 const store = createStore(reducer);
 
